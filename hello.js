aa1bcc4762b23f8aa2d0034644d85e1055a4bebc
Lester-Toribio <toribiotitit18@gmail.com>
2025-08-28 08:53:25 +0800

    added multiplication, subtraction, division

--- hello.js
+++ hello.js
@@ -3,4 +3,13 @@ num1 = 2;
 num2= 3;
 
 sum = num1 + num2;
-console.log(sum);
+diff = num1 - num2;
+prod = num1 * num2;
+quo = num1 / num2;
+
+
+
+console.log("The sum of " + num1 + " and " + num2 + " is = " + sum);
+console.log("The difference between " + num1 + " - " + num2 +" is = " +  diff );
+console.log("The product of"+ num1 + " and " +num2 +"= " +prod); 
+console.log("The quotient of "+ num1+ "and "+num2 +"=" + quo);
