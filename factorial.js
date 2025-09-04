faeedf895b71a88782d6b1f00ecc988755d69b62
Lester-Toribio <toribiotitit18@gmail.com>
2025-09-04 10:23:55 +0800

    added readline interface

--- factorial.js
+++ factorial.js
@@ -1,6 +1,17 @@
 console.log("factorial application");
- console.log("1. say hello");
+const readline = require("readline");
+const rl = readline.createInterface({
+    input: process.stdin,
+    output: process.stdout
+}
+
+)
+
+
+
+
+
+console.log("1. say hello");
   console.log("2.compute factorial");
    console.log("3.exit");
 
-   
