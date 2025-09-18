57099faba2242f844d5ece9ec1596096849c194e
Lester-Toribio <toribiotitit18@gmail.com>
2025-09-18 08:49:38 +0800

    added add item and total attack

--- hero.js
+++ hero.js
@@ -5,14 +5,33 @@ class Hero {
         this.name = name;
         this.#health = health;
         this.attack = attack;
+        this.items = [];
     }
+
     gethealth() {
         return this.#health;
 }
+getAttack(){
+    console.log(this.attack);
+}
+getName(){
+    console.log(this.name);
+}
+
+
 getStats(){
+    console.log("\n");
     console.log("Name: "+ this.name);
      console.log("Health: "+ this.#health);
  console.log("Attack: "+ this.attack);
+}
+
+addItem(item){
+    this.items.push(item);
+}
+
+totalAttack(){
+    return this.attack +this.items.reduce((sum,item) => sum+ item.bonusAttack,0);
 }
 }
 class Warrior extends Hero {
@@ -28,25 +47,38 @@ class Mage extends Hero {
     useAbility() {
         console.log(`${this.name} cast sarapness!`)
 }}
-function performAbility(hero){
-    hero.useAbility();
+
+
+class Item{
+    constructor(name,bonusAttack){
+        this.name = name;
+        this.bonusAttack = bonusAttack;
+    }
 }
+const sword = new Item ("Sword",5);
+const staff = new Item ("Staff",3);
 
 
+const kingkong = new Warrior ("kingkong",100,50);
 
-const kingkong = new Warrior("kingkong", 100, 10);
-kingkong.useAbility();
-kingkong.getStats();
+kingkong.getName();
+kingkong.getAttack();
 
-const magicsarap  = new Mage("magic sarap", 80, 5, 50);
-magicsarap.useAbility();
-magicsarap.getStats();
+kingkong.addItem(sword);
+console.log(kingkong.totalAttack());
+kingkong.addItem(sword);
+console.log(kingkong.totalAttack());
+kingkong.addItem(sword);
+console.log(kingkong.totalAttack());
+
+const magicsarap = new Mage("magicsarap",80 , 8, 50);
+
+
 
 function performAbility(hero){
     console.log("\n");
     hero.useAbility();
 }
-
-
 performAbility(kingkong);
 performAbility(magicsarap);
+
